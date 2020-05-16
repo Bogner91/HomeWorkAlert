87d027202130a943a4243e7bd6eb4b2dc359a456
Denis <7cribedream@gmail.com>
2020-05-16 21:00:04 +1000

    Initial Commit

---
+++ alerthomeworktwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alerthomeworktwo
+//
+//  Created by Denis Mashkov on 16.05.2020.
+//  Copyright © 2020 Denis Mashkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
